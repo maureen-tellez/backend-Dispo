ae7899a550ae770a7f29025bcb9f60e982b4146f
maureen-tellez <maureentellez0816@gmail.com>
2025-08-29 00:05:52 -0600

    clase jueves 28

--- routes/tareas.py
+++ routes/tareas.py
@@ -8,3 +8,28 @@ tareas_bp = Blueprint('tareas', __name__)
 @tareas_bp.route('/obtener',methods=['GET'])
 def get():
     return jsonify({"mensaje":"Estas son tus tareas"})
+
+#crear endpoint con post, recibiendo datos desde el body
+@tareas_bp.route('/crear',methods=['POST'])
+def crear():
+
+    #obtener los datos del body
+    data=request.get_json()
+
+    nombre = data.get('nombre')
+    apellido = data.get('apellido')
+    return  jsonify({"saludos":f"Hola {nombre} {apellido} como estas"})
+
+#endpoint usando put y pasando dato spor le body y el url
+
+@tareas_bp.route('/modificar/<int:user_id>',methods=['PUT'])
+def modificar(user_id):
+    #obtenemos loas datos del body 
+    data = request.get_json()
+    nombre = data.get('nombre')
+    apellido = data.get('apellido')
+    mensaje = f"Usuario con el id: {user_id} y nombre: {nombre} {apellido}"
+    
+    return jsonify ({"saludo": mensaje})
+
+    
