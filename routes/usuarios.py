2ef179c16a57dc653a68a035b154b29439d51af7
maureen-tellez <maureentellez0816@gmail.com>
2025-09-11 21:27:14 -0600

    error en la tabla!!

--- routes/usuarios.py
+++ routes/usuarios.py
@@ -92,7 +92,7 @@ def datos():
     current_user= get_jwt_identity()
 
     cursor = get_db_connection()
-    query="SELECT id_usuario,nombre,email FROM USUARIOS where id_usuario =%s"
+    query="SELECT id_usuario,nombre,email FROM usuarios where id_usuario =%s"
     cursor.execute(query,(current_user,))
     usuario= cursor.fetchone()
 
